10cb603a25b6c99bf0176d725102c7f20fd52519
Sync-Gautam <gautamdoliya69@gmail.com>
2025-11-09 23:31:12 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const connection = mongoose.connect(
     "mongodb+srv://gautamdoliya69:gautamdoliya69@pharmacycluter.wxsmngq.mongodb.net/Pharmacy?retryWrites=true&w=majority&appName=PharmacyCluter",
     {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
         ssl: true
     }
 );
