44bf1dba38822e062e4a4cda565744c8637d2ce7
Sync-Gautam <gautamdoliya69@gmail.com>
2025-11-09 23:22:32 +0530

    add db url

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,12 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const connection=mongoose.connect("mongodb+srv://gautamdoliya69:gautamdoliya69@pharmacycluter.wxsmngq.mongodb.net/Pharmacy");
+const connection = mongoose.connect(
+    "mongodb+srv://gautamdoliya69:gautamdoliya69@pharmacycluter.wxsmngq.mongodb.net/Pharmacy?retryWrites=true&w=majority&appName=PharmacyCluter",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ssl: true
+    }
+);
 
-module.exports=connection
+module.exports = connection
