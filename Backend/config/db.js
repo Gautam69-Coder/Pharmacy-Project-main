5a4037deb3bb05310d3015bab3037a674a250a23
Sync-Gautam <gautamdoliya69@gmail.com>
2025-11-09 23:54:07 +0530

    add dist file in backend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 const connection = mongoose.connect(
-    "mongodb+srv://gautamdoliya69:gautamdoliya69@pharmacycluter.wxsmngq.mongodb.net/Pharmacy",
-    {
-        ssl: true
-    }
-);
+    "mongodb+srv://gautamdoliya69:gautamdoliya69@pharmacycluter.wxsmngq.mongodb.net/Pharmacy?retryWrites=true&w=majority&tls=true"
+)
+    .then(() => console.log("✅ MongoDB connected successfully"))
+    .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 module.exports = connection
