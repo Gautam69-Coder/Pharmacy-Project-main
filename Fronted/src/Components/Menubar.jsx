d0458afeaa79331b648b85e5a1cfa568964a5f8b
Gautam-College <gautamdoliya69@gmail.com>
2025-11-20 13:08:01 +0530

    Update Menubar.jsx

--- Fronted/src/Components/Menubar.jsx
+++ Fronted/src/Components/Menubar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import DownSvg from '/src/assets/icons/down.svg';
+import DownSvg from '../assets/icons/down.svg';
 
 const Menubar = () => {
   const navigate = useNavigate();
